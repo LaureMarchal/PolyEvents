2ac40ba7d979db3c73e2723b54fa873fe5df57eb
LaureMarchal <marchal.laure91@gmail.com>
2017-03-27 16:15:54 +0200

    continue to work on the CRUD Event

--- ui/event/EventController.java
+++ ui/event/EventController.java
@@ -1,9 +1,17 @@
 package ui.event;
 
+import bl.facade.EventFacade;
+import bl.model.*;
+
+import java.util.Date;
+
 public class EventController {
 
     public void onCreate() {
-
+        /*Event event = new Event(id, title, subTitle, place, description,
+        beginningTime, registrationDeadline, duration, constraints, placesNumber, price,  delayToPay, status,
+        provider, registrations, tags, messages);
+        EventFacade.getInstance().create(event);*/
     }
 
     public void onEdit() {

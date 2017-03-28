cd28659f898b12a2d175d8305e141e9db24e0c2d
Tombean <tom.somervilleroberts@gmail.com>
2017-03-28 22:55:18 +0200

    ADD :  RegistrationDAOPG : create(registration) & delete(registration)

--- persistence/RegistrationDAOPG.java
+++ persistence/RegistrationDAOPG.java
@@ -3,19 +3,50 @@ package persistence;
 import bl.dao.RegistrationDAO;
 import bl.model.Event;
 import bl.model.Registration;
-
+import persistence.connector.Connector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class RegistrationDAOPG extends RegistrationDAO {
 
     @Override
     public Registration create(Registration registration) {
-        return null;
+            try {
+                String query = "INSERT INTO Registration (consumerID, eventID, creation_time, status) VALUES (?, ?, ?, ?)";
+                Connection connection = Connector.getInstance().getConnection();
+                PreparedStatement ps = connection.prepareStatement(query);
+                ps.setString(1, registration.getConsumer().getPseudo());
+                ps.setString(2, String.valueOf(registration.getEvent().getId()));
+                ps.setString(3, new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
+                ps.setString(4, registration.getStatus());
+                ps.execute();
+                connection.close();
+                return registration;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
     }
 
     @Override
     public boolean delete(Registration registration) {
-        return false;
+        try {
+            String query = "DELETE FROM Registration WHERE consumerID = ? AND eventID = ?";
+            Connection connection = Connector.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, registration.getConsumer().getPseudo());
+            ps.setString(2, String.valueOf(registration.getEvent().getId()));
+            ps.execute();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override

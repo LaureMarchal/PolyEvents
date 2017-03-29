358dc2f38945396e313d89f011355ece70226b89
Tombean <tom.somervilleroberts@gmail.com>
2017-03-30 01:08:42 +0200

    ADD : ProviderReviewDAOPG : create / getAllReviewsForProvider / delete ADD : MessageDAOPG : finished CRUD

--- bl/dao/MessageDAO.java
+++ bl/dao/MessageDAO.java
@@ -1,9 +1,15 @@
 package bl.dao;
 
+import bl.model.Event;
 import bl.model.Message;
+
+import java.util.List;
 
 public abstract class MessageDAO {
 
     public abstract Message create(Message message, Message parent);
 
+    public abstract boolean delete(Message message);
+
+    public abstract List<Message> findAllMessagesForEvent(Event event);
 }
--- persistence/ProviderReviewDAOPG.java
+++ persistence/ProviderReviewDAOPG.java
@@ -1,9 +1,14 @@
 package persistence;
 
+import bl.dao.DAOFactory;
 import bl.dao.ProviderReviewDAO;
-import bl.model.Consumer;
-import bl.model.Provider;
-import bl.model.ProviderReview;
+import bl.model.*;
+import persistence.connector.Connector;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * PostgreSQL DAO for the provider review model
@@ -12,6 +17,62 @@ public class ProviderReviewDAOPG extends ProviderReviewDAO {
 
     @Override
     public ProviderReview create(Provider provider, Consumer consumer, ProviderReview providerReview) {
-        return null;
+        try {
+            String query = "INSERT INTO Provider_review (consumerID, providerID, rate, content) VALUES (?, ?, ?, ?)";
+            Connection connection = Connector.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, consumer.getPseudo());
+            ps.setString(2, provider.getPseudo());
+            ps.setInt(3, providerReview.getRate());
+            ps.setString(4, providerReview.getContent());
+            ps.execute();
+            connection.close();
+            return providerReview;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public List<ProviderReview> getAllReviewsForProvider(Provider provider) {
+        try {
+            List<ProviderReview> providerReviews = new ArrayList<ProviderReview>();
+            String query = "SELECT * FROM Provider_review WHERE providerID = ?";
+            Connection connection = Connector.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, provider.getPseudo());
+            ResultSet rs = ps.executeQuery(query);
+            while(rs.next()){
+                String content = rs.getString("content");
+                int rate = rs.getInt("rate");
+                //TODO fix when ConsumerDAOPG is done
+                // Unused given the current constructor of Provider : ProviderReview(String content, int rate)
+                //Consumer consumer = (Consumer)DAOFactory.getInstance().createUserDAO().read(rs.getString("consumerID"));
+                ProviderReview providerReview = new ProviderReview(content, rate);
+                providerReviews.add(providerReview);
+            }
+            return providerReviews;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean delete(Provider provider, Consumer consumer, ProviderReview providerReview) {
+        try {
+            String query = "DELETE FROM Provider_review WHERE providerID = ? AND consumerID = ?";
+            Connection connection = Connector.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, provider.getPseudo());
+            ps.setString(2, consumer.getPseudo());
+            ps.execute();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }

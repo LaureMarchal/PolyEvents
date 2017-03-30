c06e6216bb692549c73bfd08accdead71fd304b8
mehdi-fakihani <mehdi.fakihani@etu.umontpellier.fr>
2017-03-30 17:32:06 +0200

    Created a table to see all the consumers that are registered to an event.

--- ui/Launcher.java
+++ ui/Launcher.java
@@ -20,6 +20,6 @@ public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Controller.getInstance().setStage(primaryStage);
-        Controller.getInstance().goTo(View.LOGIN);
+        Controller.getInstance().goTo(View.MAIN);
     }
 }

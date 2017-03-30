85aa9369f1ffdfbb2f2127d004225eebd0a87c13
LaureMarchal <marchal.laure91@gmail.com>
2017-03-30 19:04:19 +0200

    try tableview

--- ui/event/EventListController.java
+++ ui/event/EventListController.java
@@ -2,84 +2,76 @@ package ui.event;
 
 import bl.facade.EventFacade;
 import bl.model.Event;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import ui.Controller;
 import ui.View;
-
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import java.util.List;
 /**
  * Controller for the event's list interface
  */
 public class EventListController {
 
+    public static ObservableList<Event> eventsList;
     /**
      * The box to choose if the user wants to search free events or not
      */
     @FXML
     private ComboBox pricingBox;
-
     /**
      * The box to choose if the user wants to search finished events or not
      */
     @FXML
     private ComboBox finishedEventBox;
-
     /**
      * The box to choose if the user wants to search cancelled events or not
      */
     @FXML
     private ComboBox CancelledEventBox;
-
     /**
      * The box to choose if the user wants to search full events or not
      */
     @FXML
     private ComboBox fullEventBox;
-
     /**
      * The table where are events
      */
     @FXML
     private TableView eventsTable;
-
     /**
      * The table column for the event date
      */
     @FXML
     private TableColumn beginnigDateEvent;
-
     /**
      * The table column for the event title
      */
     @FXML
     private TableColumn titleEvent;
-
     /**
      * The table column for the event deadline to register
      */
     @FXML
     private TableColumn registrationDateEvent;
-
     /**
      * The table column for the event number of places
      */
     @FXML
     private TableColumn placesEvent;
-
-
     /**
      * The table column for the event price
      */
     @FXML
     private TableColumn priceEvent;
-
     /**
      * The button to add events can only be seen by providers or administrators
      */
     @FXML
     private Button addEventButton;
-
     /**
      * The field to write the event's title or tag the user wants to search
      */
@@ -115,8 +107,22 @@ public class EventListController {
     }
 
     public void setEvents(List<Event> events) {
-        eventsTable.getItems().setAll(events);
-        eventsTable.refresh();
+        this.eventsList = FXCollections.observableList(EventFacade.getInstance().getAllEvent();
+
     }
 
+
+    /**
+     * Initialize the games table view
+     */
+    private void initializeEventsTableView(){
+        /*eventsTable.setItems(eventsList);
+        beginnigDateEvent.setCellValueFactory(cellData -> cellData.getValue().beginningDate());
+        titleEvent.setCellValueFactory(cellData -> cellData.getValue().editorProperty());
+        registrationDateEvent.setCellValueFactory(cellData -> cellData.getValue().yearReleaseProperty());
+        placesEvent.setCellValueFactory(cellData -> cellData.getValue().gameTypeLabelProperty());
+        priceEvent.setCellValueFactory(cellData -> cellData.getValue().price());
+        eventsTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> selectedEvent = newValue);*/
+    }
 }

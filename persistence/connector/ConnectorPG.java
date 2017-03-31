be1a303a47a54a7a17180fc51e93e35d4043ec93
TheoGauchoux <theo.gauchoux.pro@gmail.com>
2017-03-31 10:17:43 +0200

    Add test DB

--- persistence/connector/ConnectorPG.java
+++ persistence/connector/ConnectorPG.java
@@ -1,8 +1,11 @@
 package persistence.connector;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * A connector to a remote PostgreSQL database hosted in an Heroku App
@@ -11,25 +14,57 @@ public class ConnectorPG extends Connector {
 
     /**
      * Config to connect to the database
-     * Don't show in a public repository
      */
-    private final static String JDBC = "postgresql";
-    private final static String HOST = "ec2-54-247-82-87.eu-west-1.compute.amazonaws.com";
-    private final static String PORT = "5432";
-    private final static String DATABASE = "dfupganb0rggik";
-    private final static String USER = "icgvjargopicqc";
-    private final static String PASSWORD = "be1b94bc52f730f74a2bf306d68d8fb3dc94f755fca93c98635705020966a1fb";
-    private final static String SSL_FOOTER = "?sslmode=require";
+    private String jdbc;
+    private String host;
+    private String port;
+    private String database;
+    private String user;
+    private String password;
+    private String sslFooter;
 
     @Override
     public Connection getConnection() {
-        String url = "jdbc:" + JDBC + "://" + HOST + ":" + PORT + "/" + DATABASE + SSL_FOOTER;
+        if (jdbc == null) {
+            loadConfiguration();
+        }
+        String url = "jdbc:" + jdbc + "://" + host + ":" + port + "/" + database + sslFooter;
         try {
-            return DriverManager.getConnection(url, USER, PASSWORD);
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Please check if you have the jdbc postgresql driver .jar");
             return null;
+        }
+    }
+
+    /**
+     * Load configuration from property file according to the DB mode (prod or test)
+     */
+    public void loadConfiguration() {
+        Properties prop = new Properties();
+        String propFileName = ".env_" + this.dbmode.name().toLowerCase();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+        try {
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                System.err.println("property file '" + propFileName + "' not found in the classpath");
+            }
+            this.jdbc = prop.getProperty("jdbc");
+            this.host = prop.getProperty("host");
+            this.port = prop.getProperty("port");
+            this.database = prop.getProperty("database");
+            this.user = prop.getProperty("user");
+            this.password = prop.getProperty("password");
+            this.sslFooter = prop.getProperty("sslFooter");
+        } catch (Exception e) {
+            System.err.println("can't read property file");
+        }
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
